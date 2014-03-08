30283997182dd31bd8b33bb69d3af4f648d67d51
Sebastian Hebel <sebah1989@gmail.com>
2014-03-08 11:34:21 +0100

    added List class

---
+++ spec/views/lists/edit.html.haml_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe "lists/edit" do
+  before(:each) do
+    @list = assign(:list, stub_model(List,
+      :name => "MyString",
+      :description => "MyString"
+    ))
+  end
+
+  it "renders the edit list form" do
+    render
+
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "form[action=?][method=?]", list_path(@list), "post" do
+      assert_select "input#list_name[name=?]", "list[name]"
+      assert_select "input#list_description[name=?]", "list[description]"
+    end
+  end
+end
