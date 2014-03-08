06ae986995df3af971f0d116a331bffd0ac58979
Piotr Palek <piotrpalek@gmail.com>
2014-03-08 13:01:24 +0100

    add user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,8 @@
 class SessionsController < ApplicationController
   def create
+    auth = request.env["omniauth.auth"]
+    user = User.find_or_create_by(uid: auth["uid"]) || User.create_with_omniauth(auth)
+    session[:user_id] = user.id
+    redirect_to root_url, :notice => "Signed in!"
   end
 end
