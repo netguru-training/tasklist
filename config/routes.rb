b31ae2aba4da1f951a4f26e63420abfd24b1727b
Piotr Palek <piotrpalek@gmail.com>
2014-03-08 12:10:04 +0100

    add sessions#create route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Tasklist::Application.routes.draw do
   resources :lists
 
+  get "/auth/:provider/callback" => "sessions#create"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
