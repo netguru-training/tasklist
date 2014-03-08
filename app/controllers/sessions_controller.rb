46dd5a96bd806377d314cf29dd37f824869c7b61
Piotr Palek <piotrpalek@gmail.com>
2014-03-08 13:12:03 +0100

    use create_with_omniauth for user creation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,12 @@
 class SessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
-    user = User.find_or_create_by(uid: auth["uid"]) || User.create_with_omniauth(auth)
+    user = begin
+      User.find_by(uid: auth["uid"])
+    rescue Mongoid::Errors::DocumentNotFound
+      User.create_with_omniauth(auth)
+    end
+
     session[:user_id] = user.id
     redirect_to root_url, :notice => "Signed in!"
   end
