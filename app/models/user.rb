d9425942261466c2c6676991d81d03fbfb1b3a95
Piotr Palek <piotrpalek@gmail.com>
2014-03-08 13:12:30 +0100

    add additional fields and create_with_omniauth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User
   include Mongoid::Document
   field :name, type: String
+  field :email, type: String
+  field :uid, type: String
+
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.uid = auth["uid"]
+      user.name = auth["info"]["name"]
+      user.email = auth["info"]["email"]
+    end
+  end
 end
