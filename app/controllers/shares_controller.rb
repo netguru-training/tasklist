709c5d4c39ae1bf83087e3e22dbd2e285c1dca7d
Piotr Palek <piotrpalek@gmail.com>
2014-03-09 13:04:40 +0100

    expose list in shares controller

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -1,7 +1,7 @@
 class SharesController < ApplicationController
   expose(:uuid) { params[:uuid] }
+  expose(:list) { List.find_by(uuid: params[:uuid]) }
   def create
-    list = List.find_by(uuid: params[:uuid])
     unless current_user.all_lists.include? list
       Share.create user: current_user, list: list
     end
